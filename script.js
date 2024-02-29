09318293c33f230bd2f55a0c3b2426c63c3e14dc
valentinalegre <valuualegre15@icloud.com>
2024-02-29 03:46:49 -0300

    Mensaje descriptivo aquí

---
+++ script.js
@@ -0,0 +1,47 @@
+let menuVisible = false;
+function mostrarOcultarMenu() {
+  if (menuVisible) {
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
+}
+
+function seleccionar() {
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
+}
+
+function efectoHabilidades() {
+  var skills = document.getElementById("skills");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    let habilidades = document.getElementsByClassName("progreso");
+    habilidades[0].classList.add("javascript");
+    habilidades[1].classList.add("htmlcss");
+    habilidades[2].classList.add("photoshop");
+    habilidades[3].classList.add("wordpress");
+    habilidades[4].classList.add("drupal");
+    habilidades[5].classList.add("comunicacion");
+    habilidades[6].classList.add("trabajo");
+    habilidades[7].classList.add("creatividad");
+    habilidades[8].classList.add("dedicacion");
+    habilidades[9].classList.add("proyect");
+  }
+}
+
+function mostrarOcultarMenu() {
+  var menuMobile = document.querySelector(".menu-mobile");
+  if (menuMobile.style.display === "block") {
+    menuMobile.style.display = "none";
+  } else {
+    menuMobile.style.display = "block";
+  }
+}
+
+window.onscroll = function () {
+  efectoHabilidades();
+};
